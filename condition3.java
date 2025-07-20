cf7a32d653729e5aa0a921fe7cebbc99d72b60d4
chandan <chandankumar936672@gmail.com>
2025-07-20 19:46:19 +0530

    print if a number is even or odd

---
+++ condition3.java
@@ -0,0 +1,15 @@
+package ConditionalS;
+
+public class condition3 {
+    public static void main(String args[]){
+        // print if number is even or odd
+        int a=3;
+        if(a%2==0){
+            System.out.println("even");
+        }
+        else {
+            System.out.println("odd");
+        }
+    }
+    
+}
