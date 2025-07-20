8a120580e0d6a6034c0542527776591ee4b398a5
chandan <chandankumar936672@gmail.com>
2025-07-20 19:42:16 +0530

    print the largest of 2 number

---
+++ condition2.java
@@ -0,0 +1,17 @@
+package ConditionalS;
+
+public class condition2 {
+    public static void main(String args[]){
+        // print the largest value
+        int a=2;
+        int b=3;
+
+        if(a>b){
+            System.out.println("a is largest than b " + a);
+        }
+        else {
+            System.out.println(" b is largest than a " + b);
+        }
+    }
+    
+}
